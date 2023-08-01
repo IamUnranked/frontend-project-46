9aa81a2d8ccdf68494d43d954227be68c1d0f76b
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-01 21:05:32 +0500

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,9 @@ program
     .description('Compares two configuration files and shows a difference.')
     .helpOption('-h, --help', 'output usage information')
     .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 
 
     program.parse();
