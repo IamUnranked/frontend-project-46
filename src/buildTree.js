ee59bb463b87b5c1d46ee595802c0327189ec56f
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-03 21:58:44 +0500

    step 3

---
+++ src/buildTree.js
@@ -0,0 +1,24 @@
+import _ from "lodash";
+
+
+const buildTree = (object1, object2) => {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    const keys = _.sortBy(_.union(keys1, keys2));
+    const result = keys.reduce((acc, key) => {
+        if(!_.has(object2, key)) {
+          return `${acc}  - ${key}: ${object1[key]}\n`;
+        } 
+        if (!_.has(object1, key)) {
+          return `${acc}  + ${key}: ${object2[key]}\n`;
+        } 
+        if (object1[key] !== object2[key]) {
+          return `${acc}  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}\n`;
+        } else {
+            return `${acc}    ${key}: ${object2[key]}\n`;
+        } 
+    }, '');
+    return `{\n${result}}`;
+};
+
+export default buildTree;
