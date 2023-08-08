52e6591ff04ae22fbf91c41d01f0641db3f57c8f
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-08 20:50:19 +0500

    step 4

--- __test__/test.js
+++ __test__/test.js
@@ -1,14 +1,17 @@
 import { fileURLToPath } from 'url';
-import { path, dirname } from 'path';
+import path from 'path';
 import fs from 'node:fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const expectFile = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
 
+const data1 = getFixturePath('file1.json');
+const data2 = getFixturePath('file2.json');
+
 test('genDiff.json', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(expectFile);
+  expect(genDiff(data1, data2)).toEqual(expectFile);
 });
