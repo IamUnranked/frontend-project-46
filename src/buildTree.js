f17c83dba0b0f6e2a8eb67424910d1e78d1745b9
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-09 23:03:50 +0500

    step 6

--- src/buildTree.js
+++ src/buildTree.js
@@ -5,17 +5,22 @@ const buildTree = (object1, object2) => {
   const keys2 = Object.keys(object2);
   const keys = _.sortBy(_.union(keys1, keys2));
   const result = keys.reduce((acc, key) => {
+    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+      return buildTree(object1[key], object2[key]);
+    }
     if (!_.has(object2, key)) {
-      return `${acc}  - ${key}: ${object1[key]}\n`;
+      return [acc, { key, value: object2[key], type: 'deleted'}];
     }
     if (!_.has(object1, key)) {
-      return `${acc}  + ${key}: ${object2[key]}\n`;
+      return [acc, { key, value: object2[key], type: 'added' }];
     }
-    if (object1[key] !== object2[key]) {
-      return `${acc}  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}\n`;
+    if (object1[key] === object2[key]) {
+      return [acc, { key, value: object1[key], type: 'unchanged'}];
     }
-    return `${acc}    ${key}: ${object2[key]}\n`;
-  }, '');
+    return [acc, {
+      key, value: object1[key], newValue: object2[key], type: 'changed' 
+    }];
+  }, []);
   return `{\n${result}}`;
 };
 
