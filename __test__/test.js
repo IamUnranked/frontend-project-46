9591dc4ef2462631634d012c1fe0329d2cb0d131
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-13 20:50:18 +0500

    step 7

--- __test__/test.js
+++ __test__/test.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const expectFile = fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
+const expectFilePlain = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf-8');
 
 const data1 = getFixturePath('file1.json');
 const data2 = getFixturePath('file2.json');
@@ -18,4 +19,5 @@ const data4 = getFixturePath('file2.yaml');
 test(genDiff, () => {
   expect(genDiff(data1, data2)).toEqual(expectFile);
   expect(genDiff(data3, data4)).toEqual(expectFile);
+  expect(genDiff(data3, data2)).toEqual(expectFilePlain);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,14 +7,14 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, opts) => {
+    console.log(genDiff(filepath1, filepath2, opts.format));
   });
 
 program.parse();
