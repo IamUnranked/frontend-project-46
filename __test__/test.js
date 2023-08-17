544b2b7a6da254aad60b6daee437cdcbb0cfbe8d
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-08-17 18:52:39 +0500

    refactoring

--- __test__/test.js
+++ __test__/test.js
@@ -6,20 +6,24 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectFile = fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
-const expectFilePlain = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf-8');
-const expectFileJson = fs.readFileSync(getFixturePath('expectedJson.txt'), 'utf-8');
+const testData = [
+  ['file1.json', 'file2.json', undefined, 'expectedStylish.txt'],
+  ['file1.yaml', 'file2.yaml', undefined, 'expectedStylish.txt'],
+  ['file1.json', 'file2.json', 'stylish', 'expectedStylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'stylish', 'expectedStylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'expectedPlain.txt'],
+  ['file1.yaml', 'file2.yaml', 'plain', 'expectedPlain.txt'],
+  ['file1.json', 'file2.json', 'json', 'expectedJson.txt'],
+  ['file1.yaml', 'file2.yaml', 'json', 'expectedJson.txt'],
+];
 
-const data1 = getFixturePath('file1.json');
-const data2 = getFixturePath('file2.json');
-
-const data3 = getFixturePath('file1.yaml');
-const data4 = getFixturePath('file2.yaml');
-
-test('genDiff', () => {
-  expect(genDiff(data1, data2)).toEqual(expectFile);
-  expect(genDiff(data3, data4)).toEqual(expectFile);
-  expect(genDiff(data3, data2, 'plain')).toEqual(expectFilePlain);
-  expect(genDiff(data1, data2, 'json')).toEqual(expectFileJson);
+describe.each(testData)('%s', (file1, file2, format, expected) => {
+  test('compareDiff', () => {
+    const data1 = getFixturePath(file1);
+    const data2 = getFixturePath(file2);
+    expect(genDiff(data1, data2, format))
+      .toEqual(readFile(expected));
+  });
 });
