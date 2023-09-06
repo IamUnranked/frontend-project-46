536d5da6350393fbaaa0e580de7af531be2d259d
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-09-06 22:06:34 +0500

    refactoring stylish/plain

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -12,37 +12,31 @@ const stringify = (value) => {
 
 const getProperty = (path, key) => `${path}${key}`;
 
-const iter = (tree, path = "") => {
-  return tree.flatMap((node) => {
-    switch (node.type) {
-      case "added": {
-        return [
-          `Property '${getProperty(path, node.key)}' was added with value: ${stringify(
-            node.value
-          )}`,
-        ];
-      }
-      case "changed": {
-        return [
-          `Property '${getProperty(path, node.key)}' was updated. From ${stringify(
-            node.value1
-          )} to ${stringify(node.value2)}`,
-        ];
-      }
-      case "deleted": {
-        return [
-          `Property '${getProperty(path, node.key)}' was removed`,
-        ]
-      }
-      case "nested": {
-        return iter(node.children, `${path}${node.key}.`);
-      }
-      default: {
-        return [];
-      }
+const iter = (tree, path = "") => tree.flatMap((node) => {
+  switch (node.type) {
+    case "added": {
+      return [
+        `Property '${getProperty(path, node.key)}' was added with value: ${stringify(node.value)}`,
+      ];
     }
-  });
-};
+    case "changed": {
+      return [
+        `Property '${getProperty(path, node.key)}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`,
+      ];
+    }
+    case "deleted": {
+      return [
+        `Property '${getProperty(path, node.key)}' was removed`,
+      ]
+    }
+    case "nested": {
+      return iter(node.children, `${path}${node.key}.`);
+    }
+    default: {
+      return [];
+    }
+  }
+});
 
 const makeFormatPlain = (tree) => {
   const result = iter(tree);
